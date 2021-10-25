0f5bd491966cf6da9adec402df1cb5392af866fb
mayankchoubey <choubey@gmail.com>
2021-10-24 21:11:34 -0700

    Add files via upload

---
+++ example.ts
@@ -0,0 +1,60 @@
+import { Router } from "./mod.ts";
+import { listenAndServe } from "https://deno.land/std/http/mod.ts";
+
+const router = new Router();
+router.get("/", async (r: Request, p: Record<string, string>) => {
+  return new Response("Hello from / handler");
+});
+router.get(
+  "/users/:userId",
+  async (r: Request, p: Record<string, string>) => {
+    return new Response(
+      "Hello from /users/:userId handler, params=" +
+        Object.entries(p).join(", "),
+    );
+  },
+);
+router.put(
+  "/users",
+  async (r: Request, p: Record<string, string>) => {
+    return new Response(
+      "Hello from PUT /users handler",
+    );
+  },
+);
+router.put(
+  "/users/:userId/attachments",
+  async (r: Request, p: Record<string, string>) => {
+    return new Response(
+      "Hello from PUT /users/:userId/attachments handler, params=" +
+        Object.entries(p).join(", "),
+    );
+  },
+);
+router.get(
+  "/users/:userId/attachments/:attachmentId",
+  async (r: Request, p: Record<string, string>) => {
+    return new Response(
+      "Hello from /users/:userId handler, params=" +
+        Object.entries(p).join(", "),
+    );
+  },
+);
+router.post(
+  "/users",
+  async (r: Request, p: Record<string, string>) => {
+    return new Response(
+      "Hello from POST /users handler",
+    );
+  },
+);
+router.patch(
+  "/users/:userId",
+  async (r: Request, p: Record<string, string>) => {
+    return new Response(
+      "Hello from POST /users handler",
+    );
+  },
+);
+
+listenAndServe(":3000", async (r) => await router.route(r));
---
+++ mod.ts
@@ -0,0 +1,78 @@
+type CallbackHandler = (
+  request: Request,
+  params: Record<string, string>,
+) => Promise<Response>;
+
+const METHODS: Record<string, string> = {
+  GET: "GET",
+  HEAD: "HEAD",
+  POST: "POST",
+  PUT: "PUT",
+  DELETE: "DELETE",
+  OPTIONS: "OPTIONS",
+  TRACE: "TRACE",
+  PATCH: "PATCH",
+};
+
+export class Router {
+  private routes: Record<string, Array<any>> = {};
+
+  constructor() {
+    for (const m in METHODS) {
+      this.routes[METHODS[m]] = [];
+    }
+  }
+
+  private add(method: string, pathname: string, handler: CallbackHandler) {
+    this.routes[method].push({
+      pattern: new URLPattern({ pathname }),
+      handler,
+    });
+  }
+
+  get(pathname: string, handler: CallbackHandler) {
+    this.add(METHODS.GET, pathname, handler);
+  }
+
+  head(pathname: string, handler: CallbackHandler) {
+    this.add(METHODS.HEAD, pathname, handler);
+  }
+
+  post(pathname: string, handler: CallbackHandler) {
+    this.add(METHODS.POST, pathname, handler);
+  }
+
+  put(pathname: string, handler: CallbackHandler) {
+    this.add(METHODS.PUT, pathname, handler);
+  }
+
+  delete(pathname: string, handler: CallbackHandler) {
+    this.add(METHODS.DELETE, pathname, handler);
+  }
+
+  options(pathname: string, handler: CallbackHandler) {
+    this.add(METHODS.OPTIONS, pathname, handler);
+  }
+
+  trace(pathname: string, handler: CallbackHandler) {
+    this.add(METHODS.TRACE, pathname, handler);
+  }
+
+  patch(pathname: string, handler: CallbackHandler) {
+    this.add(METHODS.PATCH, pathname, handler);
+  }
+
+  async route(req: Request): Promise<Response> {
+    for (const r of this.routes[req.method]) {
+      if (r.pattern.test(req.url)) {
+        const params = r.pattern.exec(req.url).pathname.groups;
+        try {
+          return await r["handler"](req, params);
+        } catch (err) {
+          return new Response(null, { status: 500 });
+        }
+      }
+    }
+    return new Response(null, { status: 404 });
+  }
+}
